43887196bb028b9b5dd7fae88d4c4b148610d53f
nistelrooy1984 <nistelrooy1984@gmail.com>
2021-04-18 14:20:15 +0700

    first commit

---
+++ lib/tugo_common.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'gruf'
+require 'gruf/stackdriver_trace'
+require 'jwt'
+require 'tugo_common/grpc_service'
+require 'tugo_common/version'
+
+module TugoCommon
+  class Error < StandardError; end
+  # Your code goes here...
+end
