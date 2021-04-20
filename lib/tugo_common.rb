61c85e05a4d906d65644fff481e4783bcb3de472
thinhnh <thinhnh2@hybrid-technologies.vn>
2021-04-20 14:15:45 +0700

    add request_params_base

--- lib/tugo_common.rb
+++ lib/tugo_common.rb
@@ -5,6 +5,7 @@ require 'gruf/stackdriver_trace'
 require 'jwt'
 require 'tugo_common/grpc_service'
 require 'tugo_common/version'
+require 'tugo_common/request_params_base'
 
 module TugoCommon
   class Error < StandardError; end
