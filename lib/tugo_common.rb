6fe435ee1c213d6b98c93a15dcdb401008edb43d
thinhnh <thinhnh2@hybrid-technologies.vn>
2021-05-08 14:31:16 +0700

    add function master data

--- lib/tugo_common.rb
+++ lib/tugo_common.rb
@@ -6,6 +6,7 @@ require 'jwt'
 require 'tugo_common/grpc_service'
 require 'tugo_common/version'
 require 'tugo_common/request_params_base'
+require 'tugo_common/master_data_related/master_data'
 
 module TugoCommon
   class Error < StandardError; end
